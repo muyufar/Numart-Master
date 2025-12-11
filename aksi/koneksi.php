c422d7d96153d5fc725bd5ced589020c572bd38b
muyufar <official.muyufar@gmail.com>
2025-12-11 21:01:58 +0700

    update beli langsung

--- aksi/koneksi.php
+++ aksi/koneksi.php
@@ -2,7 +2,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$db = "numart";
+	$db = "u700125577_numart";
 
 	$conn = new mysqli($servername, $username, $password, $db);
 	date_default_timezone_set('Asia/Jakarta');
